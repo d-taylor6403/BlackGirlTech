f630b3f231a3d8635b0bf93d077456047377e17d
dannitay <dannielletaylor@outlook.com>
2021-03-20 23:56:37 -0500

    adding user route findOne and associations

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.get('/', (req, res) => {
+    Comment.findAll()
+    .then()
+})
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,16 +1,50 @@
 const router = require('express').Router();
-const { User } = require('../../models')
+const { User, Post, Comment } = require('../../models')
 
 //get all users
 router.get('/', (req, res) => {
     User.findAll({
-        attributes: { exclude: ['password']}
+        attributes: { exclude: ['password'] }
     })
-    .then(dbUserData => res.json(dbUserData))
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.get('/:id', (req, res) => {
+    User.findOne({
+        atrributes: { exclude: ['passwprd'] },
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'title', 'post_url', 'created_at']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'created_at'],
+                include: {
+                    model: Post,
+                    attributes: ['title']
+                }
+            }
+        ]
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({message: 'No user found with this id'});
+            return;
+        }
+        res.json(dbUserData);
+    })
     .catch(err => {
         console.log(err);
-        res.status(500).json(err);
-    });
+        res.status(500).json(err)
+    })
 });
 
 
